1c11f122a382572f0df19805119cf5abd534a050
yosheng_zhang <yosheng_zhang@gss.com.tw>
2025-11-16 01:38:51 +0800

    refactor: 優化注入Kernel寫法

--- src/FarmBuddy.Service/Options/OpenAIOption.cs
+++ src/FarmBuddy.Service/Options/OpenAIOption.cs
@@ -1,6 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace FarmBuddy.Api.Options;
+namespace FarmBuddy.Service.Options;
 
 public class OpenAIOption
 {
---
+++ src/FarmBuddy.Service/SemanticKernelExtension.cs
@@ -0,0 +1,32 @@
+﻿using FarmBuddy.Service.Options;
+using FarmBuddy.Service.Plugins;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+namespace FarmBuddy.Service;
+
+public static class SemanticKernelExtension
+{
+    public static IServiceCollection AddOpenAiConfiguration(this IServiceCollection serviceCollection, IConfiguration configuration)
+    {
+        // Get configuration
+        serviceCollection.AddOptions<OpenAIOption>()
+            .Bind(configuration.GetSection(nameof(OpenAIOption)));
+
+        serviceCollection.AddSingleton<IChatCompletionService>(sp =>
+        {
+            var options = sp.GetRequiredService<IOptions<OpenAIOption>>().Value;
+            return new OpenAIChatCompletionService(options.ChatModelId, options.ApiKey);
+        });
+
+        serviceCollection.AddSingleton<KernelPlugin>(sp =>
+            KernelPluginFactory.CreateFromType<WeatherForecastPlugin>(serviceProvider: sp));
+        serviceCollection.AddKernel();
+        
+        return serviceCollection;
+    }
+}
