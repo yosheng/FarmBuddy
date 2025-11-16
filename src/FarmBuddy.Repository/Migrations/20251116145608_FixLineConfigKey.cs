4608fad63f300c838eaa309d957e9f8d5dd0cc31
yosheng_zhang <yosheng_zhang@gss.com.tw>
2025-11-16 22:57:01 +0800

    fix: 修正配置字段錯誤

---
+++ src/FarmBuddy.Repository/Migrations/20251116145608_FixLineConfigKey.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FarmBuddy.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixLineConfigKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "SystemSetting",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Key",
+                value: "LineConfig:ChannelId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "SystemSetting",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Key",
+                value: "LineConfig:ChannelID");
+        }
+    }
+}
